1fca34c999ab2172304444ddcf0b8bbcd2f44288
codebeautyprogramming <codebeautyprogramming@gmail.com>
2024-09-12 23:55:26 +0200

    Startup code for this chapter

---
+++ BankAccountsApp/Form1.cs
@@ -0,0 +1,55 @@
+namespace BankAccountsApp
+{
+    public partial class Form1 : Form
+    {
+        List<BankAccount> BankAccounts = new List<BankAccount>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateAccountBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(OwnerTxt.Text))
+                return;
+
+            BankAccount bankAccount = new BankAccount(OwnerTxt.Text);
+            BankAccounts.Add(bankAccount);
+
+            RefreshGrid();
+            OwnerTxt.Text = string.Empty;
+        }
+
+        private void RefreshGrid()
+        {
+            BankAccountsGrid.DataSource = null;
+            BankAccountsGrid.DataSource = BankAccounts;
+        }
+
+        private void DepositBtn_Click(object sender, EventArgs e)
+        {
+            if (BankAccountsGrid.SelectedRows.Count == 1)
+            {
+                BankAccount selectedBankAccount = BankAccountsGrid.SelectedRows[0].DataBoundItem as BankAccount;
+
+                string message=selectedBankAccount.Deposit(AmountNum.Value);
+                RefreshGrid();
+                AmountNum.Value = 0;
+                MessageBox.Show(message);
+            }
+        }
+
+        private void WithdrawBtn_Click(object sender, EventArgs e)
+        {
+            if (BankAccountsGrid.SelectedRows.Count == 1)
+            {
+                BankAccount selectedBankAccount = BankAccountsGrid.SelectedRows[0].DataBoundItem as BankAccount;
+
+                string message = selectedBankAccount.Withdraw(AmountNum.Value);
+                RefreshGrid();
+                AmountNum.Value = 0;
+                MessageBox.Show(message);
+            }
+        }
+    }
+}
